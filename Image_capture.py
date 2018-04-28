8cfce8d9828341c9c892f5ba0b9f6d529fa6ccc3
Benjamin Taylor <drtayloratuwe@gmail.com>
2018-04-28 23:35:25 +0100

    Adds temp humidity data to image

--- Image_capture.py
+++ Image_capture.py
@@ -2,9 +2,10 @@ import time
 import picamera 
 import datetime
 import RPi.GPIO as GPIO
+import Adafruit_DHT
+
 
 camera = picamera.PiCamera()
-
 
 def relay_on():
   GPIO.setmode(GPIO.BCM) 
@@ -18,9 +19,12 @@ def relay_off():
   print "IR OFF"
 
 def take_pic():
+	 sensor = Adafruit_DHT.DHT11
+ 	 pin = 14
+  	 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 	 camera.resolution = (1280, 720)
 	 timestamp = (str(datetime.datetime.now()))
-	 camera.annotate_text = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+	 camera.annotate_text = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + (' HeatMat={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
 	 time.sleep(4)
 	 camera.capture("/home/pi/Gecko_main/image_bin/" + timestamp + ".jpg")
 	 print "Image captured: " + timestamp
